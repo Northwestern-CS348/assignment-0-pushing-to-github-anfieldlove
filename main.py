5d2eb6221d84369e4b2d7b56f921d0f3607364bb
anfieldlove <46535489+anfieldlove@users.noreply.github.com>
2019-01-16 17:06:15 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import student_code
 class SetupTest(unittest.TestCase):
 
 	def test01(self):
+		
 		result = student_code.shouldReturnTrue()
+		
 		self.assertTrue(result)
 
 if __name__ == '__main__':
